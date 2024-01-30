b02943486b3d3c6d73a0b79faf762c42de019798
Surya <80670534+suryaln@users.noreply.github.com>
2024-01-30 10:21:27 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,12 @@
 #!/bin/bash
-#gonna push image  to docker hub repository in this deploy.
+#gonna push image  to docker hub repository in this deploy.sh 
 
 docker-compose up -d
 
-#stagging the image to docker hub registry
-             #docker tag reactimage suryaln/capstonereactimage
-
+#stagging the image to docker hub registry to dev repo
 docker tag reactimage  suryaln/dev          
 
 #pushing image to dockerhub registry
-  
 docker push suryaln/dev
 
-#docker push suryaln/capstonereactimage
-
-echo "The image has been pushed to DockerHub Registry!!! "     
-
-
-#docker tag local-image:tagname new-repo:tagname
-#docker push new-repo:tagname
-
-#Replace Tagname with desired image  Proximate
-
-#docker push suryaln/dev:tagname
+echo "The Image has been pushed to Dev Repository in DockerHub Registry!!! "     
