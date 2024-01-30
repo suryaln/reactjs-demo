51c346ec72facf9ee0b153d7f47bee83068e0800
Surya <80670534+suryaln@users.noreply.github.com>
2024-01-30 10:44:02 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ docker-compose up -d
 #stagging the image to docker hub registry to dev repo
 docker tag reactimage  suryaln/dev          
 
-#pushing image to dockerhub registry
+#pushing image to dockerhub registry 
 docker push suryaln/dev
 
 echo "The Image has been pushed to Dev Repository in DockerHub Registry!!! "     
