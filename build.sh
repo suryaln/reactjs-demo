c35a4ace7dedab930eb2b6309cbcf8b42e237234
Surya <80670534+suryaln@users.noreply.github.com>
2024-02-02 01:46:38 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ docker images
 #stoping conatiners which are running
 docker stop $(docker ps -aq)
 #removing containers
-docker rm $(docker ps -aq)
+docker rm $(docker ps -aq) 
 
 #gonna build docker image
 docker build -t reactimage .
